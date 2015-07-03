47c86cef1387561601b3eee1b310b5f49121384c
Chris Erwin <crerwin@gmail.com>
2015-07-03 17:57:47 -0400

    first commit

---
+++ dupfinder.py
@@ -0,0 +1,31 @@
+__author__ = 'Chris Erwin'
+
+import os
+import sys
+import getopt
+import hashlib
+
+def getHash(path, blocksize = 65536):
+    #this only works for files that fit in Python's working memory
+    #instead used block reading as found here: http://www.pythoncentral.io/finding-duplicate-files-with-python/
+    #return hashlib.md5(open(path, 'rb').read()).hexdigest()
+    file = open(path, 'rb')
+    hasher = hashlib.md5()
+    buffer = file.read(blocksize)
+    while len(buffer) > 0:
+        hasher.update(buffer)
+        buffer = file.read(blocksize)
+    file.close()
+    return hasher.hexdigest()
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        folders = sys.argv[1:]
+        for path in folders:
+            print(path)
+            for root, dirs, files in os.walk(path):
+                for file in files:
+                    hash = getHash(os.path.join(root, file))
+                    print(os.path.join(root, file), hash)
+    else:
+        print("Usage: dupfinder.py folder [folder2 folder3...]")
